40ce6687adf1117d3350890c05837440adacc4b8
LeanneCW <leannechristinawitzke@gmail.com>
2024-03-28 20:54:53 -0400

    add multiple questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import QOD from './QOD.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    <QOD />
-    </>
-  )
+function App() {
+
+
+
+
+ return (
+   <>
+   <QOD />
+   </>
+ )
 }
+
 
 export default App
